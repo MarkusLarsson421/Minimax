a7c41bfdd48663dc15df30df2118b5b35e693420
Markus <markus.larsson421@gmail.com>
2022-03-08 12:52:02 +0100

    Implemented a Piece class and changed all effected code to utilize the class.
    Fixed the win condition, but it still has one major bug left.

--- src/FiveInARow.java
+++ src/FiveInARow.java
@@ -3,21 +3,17 @@ import java.util.Random;
 class FiveInARow{
 	private static final int BOARD_SIZE = 7; //standard: 19
 	private static final int BOARD_CHAR_SIZE = BOARD_SIZE * 2 + 1;
+	private static final int PIECES_IN_A_ROW = 5;
 	
 	private static final char PLAYER = 'X';
 	private static final char OPPONENT = 'O';
-	
-	private static final byte EMPTY_TILE = 0;
-	private static final byte PLAYER_BYTE = 1;
-	private static final byte OPPONENT_BYTE = 2;
+
 	private static final Random rng = new Random();
-	private static final int PIECES_IN_A_ROW = 5;
 
 	private final InputManager input = new InputManager();
-	private final Byte[][] board = new Byte[BOARD_SIZE][BOARD_SIZE];
-	
-	private static boolean gameOnGoing = true;
-	
+	private final Piece[][] board = new Piece[BOARD_SIZE][BOARD_SIZE];
+	private int pieces = 0;
+
 	public FiveInARow(){
 		boolean isRunning = true;
 		while(isRunning){
@@ -38,79 +34,105 @@ class FiveInARow{
 	private void gameLoop(){
 		System.out.println("You start, type in the row and thereafter the column where you would like to place your piece.");
 		resetBoard();
-		
+		Piece piece;
+
+		boolean gameOnGoing = true;
 		while(gameOnGoing){
 			drawBoard();
-			
+
+			//Could possibly be made more readable.
+
 			//Ask user where to place their piece.
-			boolean playerWon = getUserPlacement();
-			if(playerWon){
-				gameOnGoing = false;
+			piece = getUserPlacement();
+			if(checkWin(piece)){
+				System.out.println("YOU'VE WON! CONGRATULATIONS! YOU'VE WON! YOU'VE WON!");
 				drawBoard();
+				gameOnGoing = false;
 			}else{
 				//Let the AI opponent choose where to place their piece.
-				createPiece(rng.nextInt(BOARD_SIZE) + 1, rng.nextInt(BOARD_SIZE) + 1, OPPONENT_BYTE);
+				piece = minimax();
+				//Bug with opponent, only requires 3 in a row to win!?!?! Should be 5 like it is for the player.
+				if(checkWin(piece)){
+					System.out.println("YOU'VE LOST! CONGRATULATIONS! YOU'VE LOST! YOU'VE LOST!");
+					drawBoard();
+					gameOnGoing = false;
+				}
+			}
+			if(pieces >= BOARD_SIZE * BOARD_SIZE){
+				System.out.println("YOU'VE DRAWN! CONGRATULATIONS! YOU'VE DRAWN! YOU'VE DRAWN!");
+				gameOnGoing = false;
+				drawBoard();
 			}
 		}
 	}
-	
-	private boolean getUserPlacement(){
-		boolean validPlacement = false;
+
+	private Piece minimax() {
 		int row;
 		int column;
+		boolean spotAvailable;
+
+		do{
+			row = rng.nextInt(BOARD_SIZE);
+			column = rng.nextInt(BOARD_SIZE);
+
+			spotAvailable = board[row][column] == null;
+		}while(!spotAvailable);
+		return createPiece(OPPONENT, row, column);
+	}
+
+	private Piece getUserPlacement(){
+		int row;
+		int column;
+		boolean spotAvailable;
 
 		do{
 			row = limitRange("What row");
 			column = limitRange("What column");
-			
-			validPlacement = createPiece(row, column, PLAYER_BYTE);
-		}while(!validPlacement);
 
-		if(checkWin(PLAYER_BYTE, row, column)){
-			System.out.println("YOU'VE WON! CONGRATULATIONS! YOU'VE WON! YOU'VE WON!");
-			return true;
-		}
+			spotAvailable = board[row][column] == null;
+			if(!spotAvailable){System.out.println("Error: You can't place it there, another piece is already there!");}
+		}while(!spotAvailable);
 
-		return false;
+		return createPiece(PLAYER, row, column);
 	}
 	
 	private int limitRange(String question){
 		int placement;
 		do{
 			placement = input.getInt(question);
-			if(placement > BOARD_SIZE || placement <= 0){System.out.println("Error: Your placement can not be outside the board!");}
-		}while(placement > BOARD_SIZE || placement <= 0);
+			placement--;
+			if(placement > BOARD_SIZE || placement < 0){System.out.println("Error: Your placement can not be outside the board!");}
+		}while(placement > BOARD_SIZE || placement < 0);
 		return placement;
 	}
 	
-	private boolean createPiece(int row, int column, byte player){
-		row -= 1;
-		column -= 1;
-		System.out.println("Row: " + row + ", column: " + column + ", player: " + player);
-		if(board[row][column] == 0){
-			board[row][column] = player;
-			return true;
-		}else{
-			System.out.println("Error: You can't place it there, another piece is already there!");
-			return false;
-		}
+	private Piece createPiece(char player, int row, int column){
+		Piece piece = new Piece(player, row, column);
+		board[row][column] = piece;
+		pieces++;
+		return piece;
 	}
 	
 	private void resetBoard(){
+		pieces = 0;
 		for(int i = 0; i < BOARD_SIZE; i++){
 			for(int j = 0; j < BOARD_SIZE; j++){
-				board[i][j] = EMPTY_TILE;
+				board[i][j] = null;
 			}
 		}
 	}
 	
-	private boolean checkWin(byte player, int row, int column){
-		int diagonalTopLeft = countRow(player, row, column, -1, 1);
-		int diagonalTopRight = countRow(player, row, column, 1, 1);
-		int horizontal = countRow(player, row, column, 1, 0);
-		int vertical = countRow(player, row, column, 0, 1);
+	private boolean checkWin(Piece piece){
+		if(pieces <= 8){
+			return false;
+		}
 
-		return max(diagonalTopLeft, diagonalTopRight, horizontal, vertical) > PIECES_IN_A_ROW;
+		int diagonalTopLeft = countRow(piece, -1, 1);
+		int diagonalTopRight = countRow(piece, 1, 1);
+		int horizontal = countRow(piece, 1, 0);
+		int vertical = countRow(piece, 0, 1);
+
+		return max(diagonalTopLeft, diagonalTopRight, horizontal, vertical) >= PIECES_IN_A_ROW;
 	}
 
 	private int max(int... numbers){
@@ -123,17 +145,25 @@ class FiveInARow{
 		return output;
 	}
 
-	private int countRow(byte player, int row, int column, int dirX, int dirY) {
+	private int countRow(Piece piece, int dirX, int dirY) {
 		int concurrentPieces = 1;
+		int row = piece.getRow();
+		int column = piece.getColumn();
+		char player = piece.getPlayer();
 
-		//Could possibly be improved.
-		while(row >= 0 && row < BOARD_SIZE && column >= 0 && column < BOARD_SIZE && board[row][column] == player){
+		//These two while loops could be improved.
+		while(row >= 0 && row < BOARD_SIZE && column >= 0 && column < BOARD_SIZE && board[row][column] != null && board[row][column].getPlayer() == player){
 			concurrentPieces++;
 			row += dirX;
 			column += dirY;
 		}
-		while(row >= 0 && row < BOARD_SIZE && column >= 0 && column < BOARD_SIZE && board[row][column] == player){
-			concurrentPieces++;
+
+		//Could possibly just subtract the current row and column values with concurrentPieces.
+		row = piece.getRow();
+		column = piece.getColumn();
+
+		while(row >= 0 && row < BOARD_SIZE && column >= 0 && column < BOARD_SIZE && board[row][column] != null && board[row][column].getPlayer() == player){
+			concurrentPieces	++;
 			row -= dirX;
 			column -= dirY;
 		}
@@ -160,22 +190,12 @@ class FiveInARow{
 		
 		StringBuilder output = new StringBuilder(displayRow);
 		for(int i = 0; i < BOARD_SIZE; i++){
-			output.append(tile(row, i));
+			Piece piece = board[row][i];
+			output.append(" ").append(piece == null ? ' ' : piece.getPlayer()).append(" |");
 		}
 		output.append(System.lineSeparator());
 		
 		return output.toString();
-	}
-	
-	private String tile(int row, int column){
-		byte b = board[row][column];
-		String output = " ";
-		
-		if(b == 0){output += " ";}
-		else if(b == 1){output += PLAYER;}
-		else if(b == 2){output += OPPONENT;}
-		
-		return output + " |";
 	}
 	
 	private String boardTopFrame(){
@@ -197,27 +217,5 @@ class FiveInARow{
 		String end = "+";
 		String middle = "---+".repeat(BOARD_SIZE - 1) + "---";
 		return start + middle + end + System.lineSeparator();
-	}
-
-	private class Piece{
-		private final char player;
-		private final int row;
-		private final int column;
-
-		Piece(char player, int row, int column){
-			this.player = player;
-			this.row = row;
-			this.column = column;
-		}
-
-		public char getPlayer() {
-			return player;
-		}
-		public int getRow(){
-			return row;
-		}
-		public int getColumn(){
-			return column;
-		}
 	}
 }
---
+++ src/Piece.java
@@ -0,0 +1,24 @@
+public class Piece{
+    private final char player;
+    private final int row;
+    private final int column;
+
+    Piece(char player, int row, int column){
+        this.player = player;
+        this.row = row;
+        this.column = column;
+    }
+
+    public char getPlayer() {
+        return player;
+    }
+    public int getRow(){
+        return row;
+    }
+    public int getColumn(){
+        return column;
+    }
+    public boolean equals(Piece p){
+        return player == p.getPlayer() && row == p.getRow() && column == p.getColumn();
+    }
+}
